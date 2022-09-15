ce749768cdd90dfd472a88ff1402489e3e4b9bf1
joluvana <martenhenrik@gmail.com>
2022-09-15 18:17:03 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// This is a comment to my code
+
+// rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");// output
+string userName = Console.ReadLine();// input
+Console.WriteLine("Hello" +", " + userName + "!"); // output
+Console.WriteLine($"Hello, {userName}!"); // output
